b20d1a1aa27fea67b5b04fcb315ab966e308e210
adm_user2001 <yuquilemajoel@gmail.com>
2024-01-12 19:08:50 -0500

    Cambio Tab1 y Tab2

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,81 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonButton,
+  IonCard,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCardContent,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonIcon,
+  IonInput,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { person, lockClosed, star, logIn, personAdd } from "ionicons/icons";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Registro</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent fullscreen color="primary" className="ion-padding">
+        <IonCard color={"dark"} >
+          <br />
+          <center>
+            <img alt="Imagen" src="./assets/images/login.jpg" width={100}></img>
+            <br />
+          </center>
+          <br />
+          <IonCardContent color="primary">
+            <IonGrid fixed={true}>
+              <IonRow>
+                <IonCol>
+                  <IonIcon aria-hidden="true" icon={person} size="small" />
+                </IonCol>
+                <IonCol size="auto">
+                  <IonInput
+                    placeholder="Ingrese Usuario"
+                    maxlength={20}
+                  ></IonInput>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+            <IonGrid fixed={true}>
+              <IonRow>
+                <IonCol>
+                  <IonIcon aria-hidden="true" icon={lockClosed} size="large" />
+                </IonCol>
+                <IonCol size="auto">
+                  <IonInput
+                    placeholder="Ingrese Clave"
+                    type="password"
+                    value=""
+                  ></IonInput>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+            <IonButton color="secondary" expand="block" shape="round">
+              Ingresar
+              <IonIcon slot="start" icon={logIn}></IonIcon>
+            </IonButton>
+            <br />
+            <br />
+            <br />
+            <center>
+              <h6>
+                {" "}
+                Por su seguridad NO comparta su usuario y clave con terceros
+              </h6>
+            </center>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,69 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab2.css";
+import {
+  IonList,
+  IonItem,
+  IonInput,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardContent,
+  IonIcon,
+  IonButton,
+} from "@ionic/react";
+import { save } from "ionicons/icons";
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Registro De Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+      <IonContent fullscreen color="dark" className="ion-padding">
+        <IonList>
+          <IonItem>
+            <IonInput label="Cédula" maxlength={10}></IonInput>
+          </IonItem>
+        </IonList>
+        <IonCard color={"dark"}>
+          <IonCardHeader>
+            <IonCardSubtitle>
+              Registro de Asistencia Estudiantil
+            </IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              <IonItem>
+                <IonInput label="Cédula" maxlength={10}></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonInput label="Nombre" maxlength={50}></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonInput label="Apellido" maxlength={50}></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonInput label="Email" maxlength={100} type="email"></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonInput label="Celular" maxlength={10}></IonInput>
+              </IonItem>
+            </IonList>
+          </IonCardContent>
+        </IonCard>
+        <IonButton color="primary" expand="block" shape="round">
+          Registrar
+          <IonIcon slot="start" icon={save}></IonIcon>
+        </IonButton>
       </IonContent>
     </IonPage>
   );
